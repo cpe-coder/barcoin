276adbd78afadf89a02d9c0c88cb8b4d78c12e77
Grian Gajila <griangajila2@gmail.com>
2025-07-07 14:22:55 +0800

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-import { ModeToggle } from "@/components/mode-toggle";
-
 export default function Home() {
 	return (
-		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-			<ModeToggle />
-		</div>
+		<div className=" bg-[url('/barcoin-bg.png')] bg-bottom bg-no-repeat bg-fixed items-center w-full min-h-screen"></div>
 	);
 }
