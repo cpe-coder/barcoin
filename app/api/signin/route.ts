3f01e893f05fa2d217894621e068aaabb32c13ac
Grian Gajila <griangajila2@gmail.com>
2025-07-07 17:34:22 +0800

    wip

--- app/api/signin/route.ts
+++ app/api/signin/route.ts
@@ -2,19 +2,16 @@ import { cookies } from "next/headers";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
-const ADMIN_USERNAME = "admin";
-const ADMIN_PASSWORD = "admin123";
-
 export async function POST(req: NextRequest) {
 	const { username, password } = await req.json();
 
-	if (username === ADMIN_USERNAME && password === ADMIN_PASSWORD) {
+	if (username !== "" && password !== "") {
 		(await cookies()).set({
-			name: "admin-auth",
+			name: "user-auth",
 			value: "true",
 			httpOnly: true,
 			path: "/",
-			maxAge: 60 * 60, // 1 hour
+			maxAge: 60 * 60,
 		});
 
 		return NextResponse.json({ message: "Logged in" }, { status: 200 });
