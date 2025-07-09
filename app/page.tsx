ed4da232b9f7352d0d88cfa691ddca47ec671da6
Grian Gajila <griangajila2@gmail.com>
2025-07-09 17:27:11 +0800

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Logo } from "@/constant";
+import { Loader } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,7 +8,12 @@ export default function Home() {
 			<div className="w-full min-h-screen items-center flex justify-center bg-white">
 				<div className="flex flex-col items-center justify-center gap-4">
 					<Image src={Logo} alt="Barcoin Logo" />
-					<div className="absolute"></div>
+					<div className="absolute animate-ping">
+						<Loader size={50} className="text-slate-500 animate-spin" />
+					</div>
+					<h1 className="text-4xl font-bold text-center text-gray-500">
+						Welcome to Barcoin
+					</h1>
 				</div>
 			</div>
 		</div>
