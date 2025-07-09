98358502d037dd15f6c2289e2992cba74c310841
Grian Gajila <griangajila2@gmail.com>
2025-07-09 17:14:16 +0800

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import { Logo } from "@/constant";
+import Image from "next/image";
+
 export default function Home() {
 	return (
 		<div className="  items-center w-full min-h-screen">
-			<div className="w-full min-h-screen items-center flex justify-center bg-white/50">
-				<div className="flex flex-col items-center justify-center gap-4"></div>
+			<div className="w-full min-h-screen items-center flex justify-center bg-white">
+				<div className="flex flex-col items-center justify-center gap-4">
+					<Image src={Logo} alt="Barcoin Logo" />
+					<div className="absolute"></div>
+				</div>
 			</div>
 		</div>
 	);
