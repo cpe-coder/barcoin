77d7ac3499ef6da23e95678a7de0ac4cf58a7568
Grian Gajila <griangajila2@gmail.com>
2025-07-09 12:48:00 +0800

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
 	return (
-		<div className=" bg-[url('/barcoin-bg.png')] bg-bottom bg-no-repeat bg-cover items-center w-full min-h-screen">
+		<div className="  items-center w-full min-h-screen">
 			<div className="w-full min-h-screen items-center flex justify-center bg-white/50">
 				<div className="flex flex-col items-center justify-center gap-4"></div>
 			</div>
