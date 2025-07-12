db8d33d1bc1dde4ac039993d020d0c310d404645
Grian Gajila <griangajila2@gmail.com>
2025-07-12 17:41:05 +0800

    setup auth with prisma and mongodb

---
+++ auth.ts
@@ -0,0 +1,13 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth from "next-auth";
+import prisma from "./lib/prisma";
+
+export const {
+	handlers: { GET, POST },
+	auth,
+	signIn,
+	signOut,
+} = NextAuth({
+	adapter: PrismaAdapter(prisma),
+	providers: [],
+});
