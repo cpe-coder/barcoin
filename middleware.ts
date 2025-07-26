a5761b3d56b8cdbbea98b9271d32f2610f396f9e
Grian Gajila <griangajila2@gmail.com>
2025-07-26 18:37:48 +0800

    wip

--- middleware.ts
+++ middleware.ts
@@ -2,17 +2,17 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-	const isAdminRoute = request.nextUrl.pathname.startsWith("/");
-	const isAdminLogin = request.nextUrl.pathname === "/sign-in";
+	const isAdminRoute = request.nextUrl.pathname.startsWith("/admin");
+	const isAdminLogin = request.nextUrl.pathname === "/admin/sign-in";
 	const isAuthenticated = request.cookies.get("admin-auth")?.value === "true";
 
 	if (isAdminRoute && !isAdminLogin && !isAuthenticated) {
-		return NextResponse.redirect(new URL("/sign-in", request.url));
+		return NextResponse.redirect(new URL("/admin/sign-in", request.url));
 	}
 
 	return NextResponse.next();
 }
 
 export const config = {
-	matcher: ["/:path*"],
+	matcher: ["/admin/:path*"],
 };
