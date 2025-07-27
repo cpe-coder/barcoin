e03bfeb8af7dca0f5f099ef0a37d2fc37176528a
Grian Gajila <griangajila2@gmail.com>
2025-07-28 01:34:18 +0800

    wip

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 import { FC } from "react";
 
 interface AuthLayoutProps {
@@ -5,6 +9,21 @@ interface AuthLayoutProps {
 }
 
 const AuthLayout: FC<AuthLayoutProps> = ({ children }) => {
+	const session = useSession();
+	const router = useRouter();
+
+	if (session.status === "loading") {
+		return (
+			<div className="bg-white flex items-center justify-center">
+				<h1 className="text-black">Loading...</h1>
+			</div>
+		);
+	}
+
+	if (session.status === "unauthenticated") {
+		router.push("/auth/signin");
+	}
+
 	return <div>{children}</div>;
 };
 
