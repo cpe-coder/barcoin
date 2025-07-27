da1599499aee28c006a3cd5cda6fe2d434f87983
Grian Gajila <griangajila2@gmail.com>
2025-07-28 01:17:40 +0800

    wip

--- components/form/SignInForm.tsx
+++ components/form/SignInForm.tsx
@@ -1,16 +1,61 @@
 "use client";
 
 import { Eye, EyeOff } from "lucide-react";
+import { signIn, useSession } from "next-auth/react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import React from "react";
+import { toast } from "sonner";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 
 const SignInForm = () => {
+	const session = useSession();
 	const [isPasswordVisible, setIsPasswordVisible] = React.useState(false);
-	const [email, setEmail] = React.useState("");
-	const [password, setPassword] = React.useState("");
-	// const [isLoading, setIsLoading] = React.useState(false);
+	const [disabled, setDisabled] = React.useState(false);
+	const [email, setEmail] = React.useState<string>("");
+	const [password, setPassword] = React.useState<string>("");
+	const [isLoading, setIsLoading] = React.useState(false);
+	const router = useRouter();
+
+	React.useEffect(() => {
+		if (email === "" || password === "") {
+			setDisabled(true);
+		} else {
+			setDisabled(false);
+		}
+
+		console.log(session);
+
+		if (session.status === "authenticated") {
+			router.push("/admin/history");
+		}
+	}, [email, password, session, router]);
+
+	const handleSubmit = async (e: React.FormEvent) => {
+		e.preventDefault();
+
+		setIsLoading(true);
+		const res = await signIn("credentials", {
+			redirect: false,
+			email,
+			password,
+		});
+
+		if (res?.ok) {
+			setIsLoading(false);
+			setEmail("");
+			setPassword("");
+			window.location.reload();
+			toast.success("Login successfully");
+		} else if (res?.status === 401) {
+			toast.error("Invalid username or password");
+			setIsLoading(false);
+		} else {
+			toast.error("Something went wrong");
+			setIsLoading(false);
+		}
+	};
 
 	const handleClick = () => {
 		setIsPasswordVisible((prev) => !prev);
@@ -48,10 +93,12 @@ const SignInForm = () => {
 					/>
 				</div>
 				<Button
+					onClick={handleSubmit}
+					disabled={disabled || isLoading}
 					className="w-full mt-8 py-2 bg-amber-300  hover:transition-all hover:duration-200 duration-200 transition-all  rounded-full hover:bg-amber-100 hover:cursor-pointer"
 					type="submit"
 				>
-					Sign In
+					{isLoading ? "Loading..." : "Sign In"}
 				</Button>
 			</form>
 			<div className="flex gap-2 pt-2 text-center justify-center items-center text-black">
